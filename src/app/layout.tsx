3d858cabd61aa2e238dc814a1d67c2b265bd9542
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-08-30 11:07:37 -0300

    Remoção de codigo não utilizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import "@/assets/styles/globals.scss";
 import { Suspense, useContext, useEffect } from 'react';
 import { SessionProvider } from "next-auth/react";
-// import useRouteChange from "@/hooks/useRouteChange";
 import { LoadingProvider } from "@/contexts/LoadingContext";
 import { LoadingContext } from "@/contexts/LoadingContext";
 
@@ -17,7 +16,6 @@ export default function RootLayout({
     <html lang="pt-br">
       <body suppressHydrationWarning={true}>
         <title>Delfos - Centro de Monitoramento de Clientes</title>
-        {isLoading && "Loading..."}
         <SessionProvider>
           <LoadingProvider>
             <Suspense fallback={<div>Loading...</div>}>
