072e90a9b54eccfc8092d9c253ebc88c45e12961
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-09-06 10:56:33 -0300

    chore: remover códigos não utilizados e adicionar styleguide de fontes e cores
    - Removidos códigos obsoletos que não serão utilizados no momento.
    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,21 @@
 'use client'
 import "@/assets/styles/globals.scss";
 import { Suspense, useContext, useEffect } from 'react';
-import { SessionProvider } from "next-auth/react";
-import { LoadingProvider } from "@/contexts/LoadingContext";
-import { LoadingContext } from "@/contexts/LoadingContext";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isLoading } = useContext(LoadingContext);
-
   return (
     <html lang="pt-br">
       <body suppressHydrationWarning={true}>
         <title>Delfos - Centro de Monitoramento de Clientes</title>
-        <SessionProvider>
-          <LoadingProvider>
-            <Suspense fallback={<div>Loading...</div>}>
-              <div id="primary">
-                {children}
-              </div>
-            </Suspense>
-          </LoadingProvider>
-        </SessionProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div id="primary">
+            {children}
+          </div>
+        </Suspense>
       </body>
     </html>
   );
