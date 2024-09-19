9eae9ec2750bba73a4c3ee00d92db441f5f08e1a
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-09-19 05:07:55 -0300

    feature: componente collapsed para exibir conteudo ou componentes collapsados

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -1,28 +1,37 @@
 "use client";
-import { Content, Title } from "@/components/Collapse/style";
+import { Content, Title, Collapser } from "@/components/Collapse/style";
 import { CollapseProps } from "@/components/Collapse/typo";
 import Tooltip from "@/components/Tooltip";
 import Icon from "@/components/Icon";
+import { useState } from "react";
 
 export default function Collapse({ 
-    children, 
-    className = '', 
-    icon,
-    title,
-    tooltip,
-    ...props 
-  }: CollapseProps) {
-    return (<Content data-component="Collapse" className={`${className} flex gap-y-6 flex-col`}>
-        <Title className='flex gap-y-3 gap-x-3 items-center'>
-            {tooltip && <Tooltip 
-                position="top"
-                description={tooltip} 
-            />}
+  children, 
+  className = '', 
+  icon,
+  title,
+  tooltip,
+  ...props 
+}: CollapseProps) {
+  const [expanded, setExpand] = useState(false); // No need for `any` type here.
 
-            {title}
+  return (
+    <Content data-component="Collapse" className={`${className} flex gap-y-6 flex-col`}>
+      <Title className='flex gap-y-3 gap-x-3 items-center'>
+        {tooltip && <Tooltip position="top" description={tooltip} />}
 
-            <Icon data="fa-solid fa-angle-down" />
-        </Title>
-        <>{children}</>
-    </Content>);
+        {title}
+
+        <Collapser onClick={() => {
+            setTimeout(function() { 
+                setExpand(!expanded)
+            });            
+        }}>
+          <Icon data={`fa-solid fa-angle-${expanded ? 'down' : 'up'}`} />
+        </Collapser>
+      </Title>
+
+      {expanded && <>{children}</>}  
+    </Content>
+  );
 }
--- src/components/Collapse/style.tsx
+++ src/components/Collapse/style.tsx
@@ -4,8 +4,18 @@ export const Content = styled.div`
 
 `;
 
+export const Collapser = styled.button`
+
+`;
+
 export const Title = styled.h3`   
     font-size: ${props => props.theme._fontSize.text.normal.normal3};
     font-weight: 600;
     font-size: ${props => props.theme._fontSize.text.normal.normal4};
+    .icon {
+        cursor: pointer;
+        &::before {
+            pointer-events: none;
+        }
+    }
 `;
